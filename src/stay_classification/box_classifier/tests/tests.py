78508a4b7dd241c29f023fa721ec4ddc3fa10c5f
m.salewski <matthewsalewski@yahoo.com>
2020-06-24 13:41:39 +0200

    Add testing, initial version

--- src/stay_classification/box_classifier/tests/tests.py
+++ src/stay_classification/box_classifier/tests/tests.py
@@ -2,6 +2,17 @@
 
 from ..box_method import *
 
-def test__get_time_ind__1():
+def test__get_time_ind():
     
-    return passed
+    t_arr = np.arange(2,10,2)
+    time_thresh = 1
+    
+    tests = \
+        [
+            get_time_ind(t_arr, 1, time_thresh, -1)==0,
+            get_time_ind(t_arr, 10, time_thresh, 1)==(t_arr.size - 1),
+            get_time_ind(t_arr, 5, time_thresh, -1)==0,
+            get_time_ind(t_arr, 5, time_thresh,  1)==(t_arr.size - 1)
+        ]
+    
+    return all(tests)
