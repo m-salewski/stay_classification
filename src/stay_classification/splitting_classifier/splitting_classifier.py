d3ee58bb9aec39b739e25f8979c6d003d9cee952
m.salewski <matthewsalewski@yahoo.com>
2020-07-22 15:04:13 +0200

    Update and clean some notebooks; include IQR and box-plots for the quick box method

--- src/stay_classification/splitting_classifier/splitting_classifier.py
+++ src/stay_classification/splitting_classifier/splitting_classifier.py
@@ -3,7 +3,7 @@ import numpy as np
 
 get_err = lambda x1, x2: np.sqrt((x1-x2)**2)
 
-def get_clusters(times, positions, dist_thresh, time_thresh, verbose=False):
+def get_clusters(t_arr, loc_arr, dist_thresh, time_thresh, verbose=False):
 
     """
     Get a list of cluster indices
@@ -16,114 +16,122 @@ def get_clusters(times, positions, dist_thresh, time_thresh, verbose=False):
     :return: int endpoint index of a region
     
     """
+    
+    # Output list of indices: [[beg., end],[beg., end], ...] 
     clusters = []
 
     m = 0
 
+    # The current cluster indices: [n_0, n_1, ... ]
     new_cluster = []
-    for n in range(0,positions.size-3):
+    
+    # Pass through the list of events
+    for n in range(0,loc_arr.size-3):
 
-        # Check the time
-        if times[n+1] <= times[n]+time_thresh:
-
-            get_y = positions[n+1]
-
+        # Check: is the time within the time thresh?
+        if t_arr[n+1] <= t_arr[n]+time_thresh:
+            event_loc = loc_arr[n+1]
         else: 
             continue
 
         # Get the current cluster mean
-        cluster_mean = np.mean(positions[m:n+1])
+        cluster_mean = np.mean(loc_arr[m:n+1])
 
         # Get the potential cluster mean    
-        new_cluster_mean = np.mean(positions[m:n+2])
+        new_cluster_mean = np.mean(loc_arr[m:n+2])
 
-        err1 = get_err(cluster_mean, get_y)
+        err1 = get_err(cluster_mean, event_loc)
         err2 = get_err(cluster_mean, new_cluster_mean)
-
-        #print(n, err1, err2, dist_thresh)
-
+        if verbose: print(n, err1, err2, dist_thresh)
+ 
+        # Checks: 
+        # 1. new event is within dist. thresh of current clust.
+        # 2. new mean - current mean is within dist. thresh.
         if  (err1 < dist_thresh) & (err2 < dist_thresh) & \
-            (n <= positions.size-5):
-
+            (n <= loc_arr.size-5):
             new_cluster.append(n)
-            #print(len(new_cluster))
         else:
+            # Save the current cluster and prepare restart
             txt = f'Trying {n} '
             app = "Nope"
             if (len(new_cluster) >= 2):
-                if (times[new_cluster[-1]]-times[new_cluster[0]] > time_thresh):
+                if (t_arr[new_cluster[-1]]-t_arr[new_cluster[0]] > time_thresh):
                     clusters.append(new_cluster)
                     app = 'closed'
-            #print(txt+app)
-
+ 
             new_cluster = []
 
+            # Update starting point
             m=n+1
             
     return clusters
 
 
-def get_iqr_masked(positions, iqr_fact = 3):
+def get_iqr_masked(loc_arr, iqr_fact = 3):
     # Calculate first and third quartiles
-    q25 = np.quantile(positions,0.25, interpolation='lower')
-    q75 = np.quantile(positions,0.75, interpolation='higher')
+    q25 = np.quantile(loc_arr,0.25, interpolation='lower')
+    q75 = np.quantile(loc_arr,0.75, interpolation='higher')
 
     # Calculate the interquartile range (IQR)
     iqr = abs(q75 - q25)
 
     #print(m, nn, np.where((yyyy[m:nn] > (q25 - iqr_fact * iqr)) & (yyyy[m:nn] < (q75 + iqr_fact * iqr))))
-    mask=np.where(  (positions > (q25 - iqr_fact * iqr)) \
-                  & (positions < (q75 + iqr_fact * iqr)))    
+    mask=np.where(  (loc_arr > (q25 - iqr_fact * iqr)) \
+                  & (loc_arr < (q75 + iqr_fact * iqr)))    
     
     return mask
 
-def get_iqr_std(positions, iqr_fact = 3):
+def get_iqr_std(loc_arr, iqr_fact = 3):
     
-    mask=get_iqr_masked(positions, iqr_fact)
+    mask=get_iqr_masked(loc_arr, iqr_fact)
     
-    return np.std(positions[mask])
+    return np.std(loc_arr[mask])
+'''
+#NOTE:
+This method is incorrect: it's very susceptible to drift since
+the accumulation of clusters is based initially on time; 
+this means temporal density will create clusters rather than
+the spatial nearness. 
+'''
+def get_batch_clusters(t_arr, loc_arr, dist_thresh, time_thresh, iqr_fact=3.0, verbose=False):
 
-
-def get_batch_clusters(times, positions, dist_thresh, time_thresh, iqr_fact=3.0, verbose=False):
-
-    max_len = positions.size-6
+    max_len = loc_arr.size-6
     
     clusters = []
 
-    ind = 0
-    
-    cluster = [0]
-    
-    last_time_point = times[cluster[0]]
 
-    m = 0    
-    
+    # Initializations
+    cluster = [0]
+    last_time_point = t_arr[cluster[0]]
+    m = 0   
+
+    ind = 0     
     while ind < max_len:
 
-        
         # Set the time buffer around the last point of the current cluster
         nn=m+1
-        while (times[nn]-last_time_point <= time_thresh) & (nn<=max_len):
+        # Build up a cluster based on temporal density:
+        # from a given point in time, 
+        # get the greatest time within the time threshold;
+        # repeat until not possible.        
+        # NOTE: this is not sensible!
+        while (t_arr[nn]-last_time_point <= time_thresh) & (nn<=max_len):
             nn+=1
-
-        #print(m,nn, positions[m:nn].shape)
         
-        if positions[m:nn].size > 1:
-            
-            # Get the current cluster std
-            cluster_std = np.std(positions[m:nn])
-            cluster_qstd = get_iqr_std(positions[m:nn],iqr_fact)
-            
+        # Measure the current cluster: std. dev.
+        if loc_arr[m:nn].size > 1:
+            cluster_std = np.std(loc_arr[m:nn])
+            cluster_qstd = get_iqr_std(loc_arr[m:nn],iqr_fact)
         else:
             cluster_std = 0.0
             cluster_qstd = 0.0
         
         nnn=nn
-        while ((positions[m:nnn].size > 1) & ((cluster_std >= dist_thresh) | (cluster_qstd >= dist_thresh))):
+        while ((loc_arr[m:nnn].size > 1) & ((cluster_std >= dist_thresh) | (cluster_qstd >= dist_thresh))):
             if verbose: print('\t\trefinement', nnn, cluster_std, cluster_qstd)
             # Get the current cluster std
-            cluster_std = np.std(positions[m:nnn])
-            cluster_qstd = get_iqr_std(positions[m:nnn],iqr_fact)
+            cluster_std = np.std(loc_arr[m:nnn])
+            cluster_qstd = get_iqr_std(loc_arr[m:nnn],iqr_fact)
             nnn-=1
 
         # Check!
@@ -134,14 +142,14 @@ def get_batch_clusters(times, positions, dist_thresh, time_thresh, iqr_fact=3.0,
         # if the std's are good, keep the cluster, and update the final time point
         if  (cluster_std < dist_thresh) & (cluster_qstd < dist_thresh) & (nnn<max_len) & (nnn==nn) :
 
-            last_time_point = times[nnn]
+            last_time_point = t_arr[nnn]
             ind = nnn
 
         else:
             txt = f'Trying {len(new_cluster)} '
             app = "Nope"
             if (len(new_cluster) >= 2):
-                if (times[new_cluster[-1]]-times[new_cluster[0]] > time_thresh):
+                if (t_arr[new_cluster[-1]]-t_arr[new_cluster[0]] > time_thresh):
                     clusters.append(new_cluster)
                     app = 'closed'
             if verbose: print(txt+app)
