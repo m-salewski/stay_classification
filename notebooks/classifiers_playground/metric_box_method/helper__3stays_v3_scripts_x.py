8990334e293944d6feb62b63fea76e93f2e981e0
m.salewski <matthewsalewski@yahoo.com>
2020-08-12 11:09:45 +0200

    Update explore_simple_cases_3stays_v3_scripts NB with some haphazard explorations; WARNING: the above NB is messy! --> go to the following commit

--- notebooks/classifiers_playground/metric_box_method/helper__3stays_v3_scripts_x.py
+++ notebooks/classifiers_playground/metric_box_method/helper__3stays_v3_scripts_x.py
@@ -1,8 +1,11 @@
 import numpy as np
 #from numpy import mean as np.mean
 
-def get_clusters_x(t_arr, loc_arr, dist_thresh, time_thresh, verbose=False):
+from helper__gaps import merge_cluster_pair, gap_criterion_3
+
+def get_clusters_xxx(t_arr, x_arr, dist_thresh, time_thresh, verbose=False):
     """
+    
     Get a list of cluster indices
 
     :param t_arr: np.array Trajectory array of timepoints
@@ -12,115 +15,57 @@ def get_clusters_x(t_arr, loc_arr, dist_thresh, time_thresh, verbose=False):
     
     :return: int endpoint index of a region
     
+    TODOs: 
+    * rename self to ``
+    * Update the debugging output
+    * rename `gap_criterion_3` to `gap_criterion` (usw)
+        * `gap_criterion` then calls `gap_criterion_1` & `gap_criterion_2`, etc. 
     """
     get_err = lambda x1, x2: abs(x1-x2) #np.sqrt((x1-x2)**2)
     
-    # Output list of indices: [[beg., end],[beg., end], ...] 
-    clusters = []
+    # Want to use this here to keep the scope variables
+    def check_and_merge_clusters(clusters):
 
-    m = 0
+        # Check whether the new cluster is can be merged with the previous one
+        c1, c2 = clusters[-2],clusters[-1]
 
-    # The current cluster indices: [n_0, n_1, ... ]
-    new_cluster = [m]
-    
-    # Pass through the list of events
-    for n in range(0,loc_arr.size-3):
-
-        # Check: is the time within the time thresh?
-        #
-        #print(m,n,'pre')
-        if abs(t_arr[n+1] - t_arr[n]) <= time_thresh:
-            event_loc = loc_arr[n+1]
-        else: 
-            print(m,n,'cont')
-            continue
-
-        if m == n:
-            continue
-        
-        # Get the current cluster mean
-        cluster_mean = np.mean(loc_arr[m:n+1])
-
-        # Get the potential cluster mean    
-        new_cluster_mean = np.mean(\
-            np.concatenate([loc_arr[m:n+1],loc_arr[n+1].reshape(1,)])\
-                )
-
-        err1 = get_err(cluster_mean, event_loc)
-        err2 = get_err(cluster_mean, new_cluster_mean)
-        
-        if verbose: print(f"{m:5d}, {n:5d}, {err1:7.3f}, {err2:7.3f}, {dist_thresh:7.3f}")
- 
-        # Checks: 
-        # 1. new event is within dist. thresh of current clust.
-        # 2. new mean - current mean is within dist. thresh.
-        if  (err1 < dist_thresh) & (err2 < dist_thresh) & \
-            (n <= loc_arr.size-5):
-            #if verbose: print("append")
-            new_cluster.append(n)
+        if gap_criterion_3(dist_thresh, time_thresh)(t_arr, x_arr,c1,c2):            
+            # Keep `min_index`? Here is it fixed
+            min_index = len(clusters)-2
+            return merge_cluster_pair(clusters, min_index).copy()  
         else:
-            # Save the current cluster and prepare restart
-            txt = f'Trying {n} '
-            app = "Nope"
-            if (len(new_cluster) >= 2):
-                # Since this is an incomplete method (clusters will be merged after), 
-                # can keep this; otherwise, would lose small clusters
-                #if (abs(t_arr[new_cluster[-1]]-t_arr[new_cluster[0]]) > time_thresh):
-                clusters.append(new_cluster)
-                app = 'closed'
+            return clusters
 
-            # Update starting point
-            m=n+1
-
-            #if verbose: print(txt+app)
-            new_cluster = [m]
-            
-    return clusters
-
-
-def get_clusters_xx(t_arr, loc_arr, dist_thresh, time_thresh, verbose=False):
-    """
-    Get a list of cluster indices
-
-    :param t_arr: np.array Trajectory array of timepoints
-    :param timepoint: float Timepoint
-    :param time_thresh: float buff around timepoint  
-    :param direction: int gets the min or max index of a region
-    
-    :return: int endpoint index of a region
-    
-    """
-    get_err = lambda x1, x2: abs(x1-x2) #np.sqrt((x1-x2)**2)
-    
+        
     # Output list of indices: [[beg., end],[beg., end], ...] 
     clusters = []
-
-    m = 0
 
     # The current cluster indices: [n_0, n_1, ... ]
     new_cluster = []
     
     # Pass through the list of events
-    #for n in range(0,loc_arr.size-3):
-    n = 0
-    while n < loc_arr.size-3:
+    #for n in range(0,x_arr.size-3):
+    # Initialize working indices
+    m, n = 0, 0
+    while n < x_arr.size-3:
+        
         # Check: is the time within the time thresh?
         if abs(t_arr[n+1] - t_arr[n]) <= time_thresh:
-            event_loc = loc_arr[n+1]
+            new_x = x_arr[n+1]
         else: 
             n+=1
             continue
 
         # Get the current cluster mean
-        cluster_mean = np.mean(loc_arr[m:n+1])
+        cluster_mean = np.mean(x_arr[m:n+1])
 
         # Get the potential cluster mean    
-        new_cluster_mean = np.mean(loc_arr[m:n+2])
+        new_cluster_mean = np.mean(x_arr[m:n+2])
 
-        err1 = get_err(cluster_mean, event_loc)
+        err1 = get_err(cluster_mean, new_x)
         err2 = get_err(cluster_mean, new_cluster_mean)
         
-        #if verbose: print(f"{n:5d}, {err1:7.3f}, {err2:7.3f}, {dist_thresh:7.3f}")
+        #if verbose: print(f"{n:5d}, {err1:6.3f}, {err2:6.3f}")
  
         # Checks: 
         # 1. new event is within dist. thresh of current clust.
@@ -142,26 +87,43 @@ def get_clusters_xx(t_arr, loc_arr, dist_thresh, time_thresh, verbose=False):
             #if verbose: print(txt+app)
             new_cluster = []
 
-            # Update starting point
-            m=n+1
+            # Check whether the new cluster is can be merged with the previous one
+            # NOTE: this combines the logic from `merge_clusters` function
+            if len(clusters) > 1:
+                clusters = check_and_merge_clusters(clusters)
+            
+            # Update starting point (TODO: n or n+1?)
+            m=n
                     
-        if verbose: print(txt, f"err1 = {err1:6.3f}, err2 = {err2:6.3f}", app)
+        #if verbose: print(txt, f"err1 = {err1:6.3f}, err2 = {err2:6.3f}", app)
         n+=1
     
+    # Since the last part of the trajectory array is possibly skipped,
+    # need to close last potential cluster
     n-=1
-    #print(n, new_cluster)
-    if len(new_cluster) > 0:
-        m = new_cluster[0]
+    
+    m = clusters[-1][-1]
+    n = m+1
+    print("0", len(new_cluster), m, n, x_arr.size)
+        
+    if (len(new_cluster) > 0):
 
-        while (n < loc_arr.size) & (new_cluster[0] < n):    
+        m = new_cluster[0]
+        n = new_cluster[-1]
+            
+        while n < x_arr.size: 
+            
+            print("0.11", m, n, len(new_cluster))
+            
             # Get the current cluster mean
-            cluster_mean = np.mean(loc_arr[m:n+1])
+            cluster_mean = np.mean(x_arr[m:n+1])
 
             # Get the potential cluster mean    
-            new_cluster_mean = np.mean(loc_arr[m:n+1])
+            new_cluster_mean = np.mean(x_arr[m:n+1])
 
-            err1 = get_err(cluster_mean, event_loc)
+            err1 = get_err(cluster_mean, new_x)
             err2 = get_err(cluster_mean, new_cluster_mean)    
+            
             if len(new_cluster) != 0:
                 txt = f'Try {n:4d}: '
                 app = ""
@@ -177,16 +139,236 @@ def get_clusters_xx(t_arr, loc_arr, dist_thresh, time_thresh, verbose=False):
                         #if (abs(t_arr[new_cluster[-1]]-t_arr[new_cluster[0]]) > time_thresh):
                         clusters.append(new_cluster)
                         app = 'closing'
+                        
+                        # Check whether the new cluster is can be merged with the previous one
+                        clusters = check_and_merge_clusters(clusters)
 
+                        # Update starting point (TODO: n or n+1?)
+                        m=n
+
+                #if verbose: print(txt, f"err1 = {err1:6.3f}, err2 = {err2:6.3f}", app)
+                n+=1 
+            if verbose: print(txt, f"err1 = {err1:6.3f}, err2 = {err2:6.3f}", app)
+            
+    elif (m < x_arr.size-1):
+            
+        new_cluster = [m]
+        
+        n = m+1
+        
+        while n < x_arr.size-1: 
+            
+            # Check: is the time within the time thresh?
+            if abs(t_arr[n+1] - t_arr[n]) <= time_thresh:
+                new_x = x_arr[n+1]
+            else: 
+                n+=1
+                continue
+
+            print("0.1", m, n, len(new_cluster))
+
+            # Get the current cluster mean
+            cluster_mean = np.mean(x_arr[m:n+1])
+
+            # Get the potential cluster mean    
+            new_cluster_mean = np.mean(x_arr[m:n+1])
+
+            err1 = get_err(cluster_mean, new_x)
+            err2 = get_err(cluster_mean, new_cluster_mean)    
+
+            if len(new_cluster) != 0:
+                txt = f'Try {n:4d}: '
+                app = ""
+                if  (err1 < dist_thresh) & (err2 < dist_thresh):
+                    if verbose: print("append")
+                    new_cluster.append(n)
+                else:
+                    # Save the current cluster and prepare restart
+                    app = "breaking"
+                    if (len(new_cluster) >= 2):
+                        # Since this is an incomplete method (clusters will be merged after), 
+                        # can keep this; otherwise, would lose small clusters
+                        #if (abs(t_arr[new_cluster[-1]]-t_arr[new_cluster[0]]) > time_thresh):
+                        clusters.append(new_cluster)
+                        app = 'closing'
+                        
+                        m = n
+                        # Check whether the new cluster is can be merged with the previous one
+                        clusters = check_and_merge_clusters(clusters)
+            else:
+                print("Empty")
             if verbose: print(txt, f"err1 = {err1:6.3f}, err2 = {err2:6.3f}", app)                
 
             n+=1
-        if len(new_cluster) > 0:
-            clusters.append(new_cluster)                
+        print("1",len(new_cluster))
+    else:
+        pass
+    
+    print("2:",len(new_cluster))
+    
+    # If there's an open cluster, add it to the list of clusters
+    if len(new_cluster) > 0:
+        clusters.append(new_cluster)   
+
+        # and check and merge it
+        clusters = check_and_merge_clusters(clusters)
     
     
-    if loc_arr.size-1 not in clusters[-1]:
-        if verbose: print(f"{loc_arr.size:4d} is not in: [{clusters[-1][0]:4d},{clusters[-1][-1]:4d}]" )
+    if x_arr.size-1 not in clusters[-1]:
+        if verbose: print(f"{x_arr.size:4d} is not in: [{clusters[-1][0]:4d},{clusters[-1][-1]:4d}]" )
+    else:
+        if verbose: print("Done")
+            
+    return clusters
+
+def get_clusters_xxxx(t_arr, x_arr, dist_thresh, time_thresh, verbose=False):
+    """
+    
+    Get a list of cluster indices
+
+    :param t_arr: np.array Trajectory array of timepoints
+    :param timepoint: float Timepoint
+    :param time_thresh: float buff around timepoint  
+    :param direction: int gets the min or max index of a region
+    
+    :return: int endpoint index of a region
+    
+    TODOs: 
+    * rename self to ``
+    * Update the debugging output
+    * rename `gap_criterion_3` to `gap_criterion` (usw)
+        * `gap_criterion` then calls `gap_criterion_1` & `gap_criterion_2`, etc. 
+        
+    ALGO:
+    1. start with m,n as [m,..,n]
+    2. Check if n+1 is within time-thresh
+        2.1. F: 
+            2.1.1. close and merge current (if current)
+            2.1.2. update m,n
+        2.2. T: Check if n+1 meets dist criteria
+            2.2.1. F: 
+                2.2.1.1. close and merge current (if current)
+                2.2.1.2. update m,n        
+            2.2.2. T:
+                2.2.1.1. add to current
+                2.2.1.2. update n        
+            
+    """
+    get_err = lambda x1, x2: abs(x1-x2) #np.sqrt((x1-x2)**2)
+    
+    # Want to use this here to keep the scope variables
+    def check_and_merge_clusters(clusters):
+
+        # Check whether the new cluster is can be merged with the previous one
+        c1, c2 = clusters[-2],clusters[-1]
+
+        if gap_criterion_3(dist_thresh, time_thresh)(t_arr, x_arr,c1,c2):            
+            # Keep `min_index`? Here is it fixed
+            min_index = len(clusters)-2
+            return merge_cluster_pair(clusters, min_index).copy()  
+        else:
+            return clusters
+
+        
+    # Output list of indices: [[beg., end],[beg., end], ...] 
+    clusters = []
+
+    # The current cluster indices: [n_0, n_1, ... ]
+    new_cluster = []
+    
+    # Pass through the list of events
+    #for n in range(0,x_arr.size-3):
+    # Initialize working indices
+    m, n = 0, 1
+        
+    new_cluster = [m]
+
+    while n < x_arr.size-1:
+        
+        # Check time between consecutive events
+        time_criterion = False
+                
+        # Check: is the time within the time thresh?
+        if abs(t_arr[n+1] - t_arr[n]) <= time_thresh:
+            if verbose: print(f"{m:5d}, {n:5d}\tTime passed")            
+            new_x = x_arr[n+1]
+            time_criterion = True
+
+        # Check distance between consecutive events
+        dist_criterion = False
+        if time_criterion:
+                    
+            new_x = x_arr[n+1].reshape(1,)
+            
+            # Get the current cluster mean
+            cluster_mean = np.mean(x_arr[m:n+1])
+
+            # Get the potential cluster mean    
+            new_cluster_mean = np.mean(\
+                np.concatenate([x_arr[m:n+1],new_x])\
+                    )
+
+            err1 = get_err(cluster_mean, new_x[0])
+            err2 = get_err(cluster_mean, new_cluster_mean)
+            
+            if  (err1 < dist_thresh) & (err2 < dist_thresh):
+                dist_criterion = True
+                if verbose: print(f"{m:5d}, {n:5d}\tDist passed: {err1:6.3f}, {err2:6.3f}")
+ 
+        if dist_criterion:
+            # both distance and time (implicitly) are okay
+            if verbose: print("\t\tAdding event")
+            new_cluster.append(n)
+            n += 1
+            continue
+            
+        elif time_criterion: 
+            # the time is okay, but distance not --> close
+            criterion = "dist. criterion failed"            
+            
+        else:
+            # Both dist and time are exceeded --> close
+            criterion = "time criterion failed"
+            
+        # Checks: 
+        # 1. new event is within dist. thresh of current clust.
+        # 2. new mean - current mean is within dist. thresh.
+
+ 
+        if verbose: print(f"\n    Closing cluster: {criterion}, {len(new_cluster)}, {time_criterion}\n")
+        if verbose: print(f"\tfrom {m:5d}, {n:5d}")
+
+        # Save the current cluster and prepare restart            
+        if ((len(new_cluster) >= 2) & (time_criterion)):
+            # Since this is an incomplete method (clusters will be merged after), 
+            # can keep this; otherwise, would lose small clusters
+            if verbose: print("\t\tnew cluster:", new_cluster)
+            clusters.append(new_cluster)
+            m = n
+        else:
+            m += 1
+        n = m+1
+        
+        if verbose: print(f"\t  to {m:5d}, {n:5d}")
+            
+        new_cluster = [m]
+
+        # Check whether the new cluster is can be merged with the previous one
+        # NOTE: this combines the logic from `merge_clusters` function
+        if len(clusters) > 1:
+            clusters = check_and_merge_clusters(clusters)
+
+    # Save the last cluster if non-empty         
+    if len(new_cluster) > 2:
+        # Since this is an incomplete method (clusters will be merged after), 
+        # can keep this; otherwise, would lose small clusters
+        if verbose: print("\t\tLast cluster:", new_cluster)
+        clusters.append(new_cluster)
+        clusters = check_and_merge_clusters(clusters)
+    
+    if x_arr.size-1 not in clusters[-1]:
+        print(new_cluster)
+        if verbose: print(f"{x_arr.size:4d} is not in: [{clusters[-1][0]:4d},{clusters[-1][-1]:4d}]" )
     else:
         if verbose: print("Done")
             
